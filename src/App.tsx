2135ceef9cffe052cd6d66b6a8fd52b99b249fc7
Veronica <no.demchenko@yandex.ru>
2025-04-17 23:22:35 +0300

    feat: получение данных из store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import { useCounterStore } from './model/counterStore';
 
 function App() {
-
+  const { counter } = useCounterStore();
   return (
-    <>
-      
-    </>
+    <div className='wrapper'>
+      <span>{counter}</span>
+    </div>
   )
 }
 
