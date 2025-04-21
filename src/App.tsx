1541a8363b51e19ca2e1c28e230b11b20bb3c433
Veronica <no.demchenko@yandex.ru>
2025-04-21 21:30:38 +0300

    feat: пример использования transfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,75 @@
-import './App.css';
+import { Transfer, TransferProps } from "antd";
+import React, { useState } from "react";
+import "./index.css";
+import { TodoItem } from "./model/todoStore";
 
-function App() {
-  
+const data: TodoItem[] = [
+  {
+    title: "Title 1",
+    isCompleted: true,
+    key: "1"
+  },
+  {
+    title: "Title 2",
+    isCompleted: true,
+    key: "2"
+  },
+  {
+    title: "Title 3",
+    isCompleted: false,
+    key: "3"
+  },
+  {
+    title: "Title 4",
+    isCompleted: true,
+    key: "4"
+  },
+];
+
+const App: React.FC = () => {
+  const [selectedKeys, setSelectedKeys] = useState<TransferProps["targetKeys"]>(
+    []
+  );
+
+  const [targetKeys, setTargetKeys] =
+    useState<TransferProps["targetKeys"]>([]);
+
+  const onChange: TransferProps["onChange"] = (
+    nextTargetKeys,
+    direction,
+    moveKeys
+  ) => {
+    console.log("targetKeys:", nextTargetKeys);
+    console.log("direction:", direction);
+    console.log("moveKeys:", moveKeys);
+    setTargetKeys(nextTargetKeys);
+  };
+
+  const onSelectChange: TransferProps["onSelectChange"] = (
+    sourceSelectedKeys,
+    targetSelectedKeys
+  ) => {
+    console.log("sourceSelectedKeys:", sourceSelectedKeys);
+    console.log("targetSelectedKeys:", targetSelectedKeys);
+    setSelectedKeys([...sourceSelectedKeys, ...targetSelectedKeys]);
+  };
+
+  const onScroll: TransferProps["onScroll"] = (direction, e) => {
+    console.log("direction:", direction);
+    console.log("target:", e.target);
+  };
+
   return (
-    <div className='wrapper'>
-      
-    </div>
-  )
-}
-
-export default App
+    <Transfer
+      dataSource={data}
+      titles={["Done", "Process"]}
+      targetKeys={targetKeys}
+      selectedKeys={selectedKeys}
+      onChange={onChange}
+      onSelectChange={onSelectChange}
+      onScroll={onScroll}
+      render={(item) => <div key={item.title}>{item.title}</div>}
+    />
+  );
+};
+export default App;
---
+++ src/model/todoStore.ts
@@ -0,0 +1,6 @@
+export type TodoItem = {
+    title: string;
+    isCompleted: boolean;
+    key: string;
+}
+
