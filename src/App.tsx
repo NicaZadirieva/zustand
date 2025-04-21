502d8434ad11d6a24288d611ccee440878fcaba7
Veronica <no.demchenko@yandex.ru>
2025-04-21 20:44:09 +0300

    refactor: удалила лишнее для todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css';
-import { addTen } from './helpers/addTen';
-import { useCounterStore } from './model/counterStore';
 
 function App() {
-  const { counter, decrement, increment } = useCounterStore();
+  
   return (
     <div className='wrapper'>
-      <span>{counter}</span>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={addTen}>add 10</button>
+      
     </div>
   )
 }
