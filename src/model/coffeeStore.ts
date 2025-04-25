776e19803dddf5ab3b1831fbc785d46c51e62389
Veronica <no.demchenko@yandex.ru>
2025-04-25 23:41:26 +0300

    feat: пример api-запроса

--- src/model/coffeeStore.ts
+++ src/model/coffeeStore.ts
@@ -1,1 +1,27 @@
+import { create, StateCreator } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { CoffeeType } from '../types/coffeeTypes';
+
 const BASE_URL = "https://purpleschool/coffee-api";
+
+type CoffeeState = {
+    coffeeList?: CoffeeType[];
+};
+
+type CoffeeActions = {
+    getCoffeeList: () => void;
+};
+
+const coffeeSlice: StateCreator<CoffeeActions & CoffeeState, [["zustand/devtools", never]]> = (set) => ({
+    coffeeList: undefined,
+    getCoffeeList: async() => {
+        try {
+            const { data } = await axios.get(BASE_URL);
+            set({coffeeList: data});
+        } catch (error) {
+            console.error(error);
+        }
+    }
+})
+
+export const useCoffeeStore = create<CoffeeActions & CoffeeState>()(devtools(coffeeSlice));
