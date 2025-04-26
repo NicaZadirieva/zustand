f49b9161d841601e6c913dcefd94a11173256014
Veronica <no.demchenko@yandex.ru>
2025-04-26 22:26:05 +0300

    refactor: форматирование

--- src/App.tsx
+++ src/App.tsx
@@ -23,33 +23,65 @@ const App: React.FC = () => {
   }
 
   
-  return (<div className="wrapper">
-    <Input placeholder='Поиск' value={text} onChange={handleSearch}/>
-    <div style={{display: "flex"}}>
-    <div className='cardsContainer'>
-      {coffeeList && coffeeList.map((coffee) => {
-        return (
-          <Card key={coffee.id} cover={<img src={coffee.image} alt={coffee.name}
-          />} actions={[<Button icon={<ShoppingCartOutlined/>} onClick={() => {saveCoffeeToOrder({ id: coffee.id, name: coffee.name, subTitle: coffee.subTitle})}}>{coffee.price}</Button>]}>
-              <Card.Meta title={coffee.name} description={coffee.subTitle}/>
-              <Tag color="purple" style={{marginTop: 12}}>{coffee.type}</Tag>
-              <Rate defaultValue={coffee.rating} disabled allowHalf/>
-          </Card>
-        )
-      })}
+  return (
+    <div className="wrapper">
+      <Input placeholder="Поиск" value={text} onChange={handleSearch} />
+      <div style={{ display: "flex" }}>
+        <div className="cardsContainer">
+          {coffeeList &&
+            coffeeList.map((coffee) => {
+              return (
+                <Card
+                  key={coffee.id}
+                  cover={<img src={coffee.image} alt={coffee.name} />}
+                  actions={[
+                    <Button
+                      icon={<ShoppingCartOutlined />}
+                      onClick={() => {
+                        saveCoffeeToOrder({
+                          id: coffee.id,
+                          name: coffee.name,
+                          subTitle: coffee.subTitle,
+                        });
+                      }}
+                    >
+                      {coffee.price}
+                    </Button>,
+                  ]}
+                >
+                  <Card.Meta
+                    title={coffee.name}
+                    description={coffee.subTitle}
+                  />
+                  <Tag color="purple" style={{ marginTop: 12 }}>
+                    {coffee.type}
+                  </Tag>
+                  <Rate defaultValue={coffee.rating} disabled allowHalf />
+                </Card>
+              );
+            })}
+        </div>
+        <aside className="cart">
+          <h1>Заказ</h1>
+          {persistedOrderList && persistedOrderList.length > 0 ? (
+            <>
+              {persistedOrderList.map((item, index) => {
+                return (
+                  <span key={index}>
+                    {item.name} {item.subTitle} - {item.quantity} шт.
+                  </span>
+                );
+              })}
+              <Input placeholder="адрес" />
+              <Button type="primary">Сделать заказ</Button>
+              <Button onClick={clearCart}>Очистить корзину</Button>
+            </>
+          ) : (
+            <span>Добавьте напитки</span>
+          )}
+        </aside>
+      </div>
     </div>
-    <aside className="cart">
-      <h1>Заказ</h1>
-      {persistedOrderList && persistedOrderList.length > 0 ? (<>
-       {persistedOrderList.map((item, index) => {
-        return <span key={index}>{item.name} {item.subTitle} - {item.quantity} шт.</span>;
-       })}
-       <Input placeholder='адрес'/>
-       <Button type="primary">Сделать заказ</Button>
-       <Button onClick={clearCart}>Очистить корзину</Button>
-      </>): <span>Добавьте напитки</span>}
-    </aside>
-    </div>
-  </div>);
+  );
 };
 export default App;
