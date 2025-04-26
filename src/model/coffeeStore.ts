5da8364e2596f64c4777bd30c314ec9da23814c8
Veronica <no.demchenko@yandex.ru>
2025-04-26 11:54:27 +0300

    feat: добавила abortController
    для отмены предыдущего запроса

--- src/model/coffeeStore.ts
+++ src/model/coffeeStore.ts
@@ -7,21 +7,35 @@ const BASE_URL = "https://purpleschool.ru/coffee-api";
 
 type CoffeeState = {
     coffeeList?: CoffeeType[];
+    controller?: AbortController;
 };
 
 type CoffeeActions = {
     getCoffeeList: (params?: GetCoffeeListReqParams) => void;
 };
 
-const coffeeSlice: StateCreator<CoffeeActions & CoffeeState, [["zustand/devtools", never]]> = (set) => ({
+const coffeeSlice: StateCreator<CoffeeActions & CoffeeState, [["zustand/devtools", never]]> = (set, get) => ({
     coffeeList: undefined,
+    controller: undefined,
     getCoffeeList: async(params?: GetCoffeeListReqParams) => {
+        const { controller } = get();
+        if (controller) {
+            controller.abort();
+        } 
+
+        const newController = new AbortController();
+        set({ controller: newController });
+        const { signal } = newController;
+        
         try {
             const { data } = await axios.get(BASE_URL, {
-                params
+                params, signal
             });
             set({coffeeList: data});
         } catch (error) {
+            if (axios.isCancel(error)) {
+                return;
+            }
             console.error(error);
         }
     }
