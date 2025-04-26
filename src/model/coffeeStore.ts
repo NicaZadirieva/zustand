97dca513a37d741d71175121bfbe964d12e59b0b
Veronica <no.demchenko@yandex.ru>
2025-04-26 22:08:16 +0300

    feat: первая реализация с persist
    добавление заказа в корзину

--- src/model/coffeeStore.ts
+++ src/model/coffeeStore.ts
@@ -1,22 +1,54 @@
 import axios from 'axios';
 import { create, StateCreator } from 'zustand';
-import { devtools } from 'zustand/middleware';
-import { CoffeeType, GetCoffeeListReqParams } from '../types/coffeeTypes';
+import { persist } from 'zustand/middleware';
+import { CoffeeType, GetCoffeeListReqParams, OrderItem } from '../types/coffeeTypes';
 
 const BASE_URL = "https://purpleschool.ru/coffee-api";
 
 type CoffeeState = {
     coffeeList?: CoffeeType[];
+    persistedOrderList?: OrderItem[];
     controller?: AbortController;
 };
 
 type CoffeeActions = {
     getCoffeeList: (params?: GetCoffeeListReqParams) => void;
+    saveCoffeeToOrder: ({ id, name } : { id: number; name: string; }) => void;
+    clearCart: () => void;
 };
 
-const coffeeSlice: StateCreator<CoffeeActions & CoffeeState, [["zustand/devtools", never]]> = (set, get) => ({
+const coffeeSlice: StateCreator<CoffeeActions & CoffeeState, [["zustand/persist", unknown]]> = (set, get) => ({
     coffeeList: undefined,
+    persistedOrderList: undefined,
     controller: undefined,
+    clearCart: () => {
+        const state = get();
+        set({...state, persistedOrderList: []});
+    },
+    saveCoffeeToOrder: ({ id, name } : { id: number; name: string; }) => {
+        const { persistedOrderList } = get();
+        let newPersistedOrderList = persistedOrderList;
+        if (newPersistedOrderList == undefined) {
+            newPersistedOrderList = [];
+        } 
+        const foundOrderItemIndex: number | undefined = newPersistedOrderList.findIndex((orderItem) => {
+            return orderItem.id == id;
+        })
+        if (foundOrderItemIndex) {
+            const foundOrderItem = newPersistedOrderList[foundOrderItemIndex];
+            newPersistedOrderList.splice(foundOrderItemIndex, 1, {
+                ...foundOrderItem,
+                quantity: foundOrderItem.quantity + 1
+            })
+        } else {
+            newPersistedOrderList.push({
+                id,
+                name,
+                quantity: 1,
+                size: "L"
+            })
+        }
+    },
     getCoffeeList: async(params?: GetCoffeeListReqParams) => {
         const { controller } = get();
         if (controller) {
@@ -41,4 +73,7 @@ const coffeeSlice: StateCreator<CoffeeActions & CoffeeState, [["zustand/devtools
     }
 })
 
-export const useCoffeeStore = create<CoffeeActions & CoffeeState>()(devtools(coffeeSlice));
+export const useCoffeeStore = create<CoffeeActions & CoffeeState>()(persist(coffeeSlice, {
+    name: 'coffeeStore',
+    partialize: (state) => ({persistedOrderList: state.persistedOrderList})
+}));
